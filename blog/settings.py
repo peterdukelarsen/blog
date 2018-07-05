fbaa0ad1dce2c5bc1b2be7d15c4b70b1c231f700
peterlarsen <plarsen@umich.edu>
2018-07-05 16:53:50 -0400

    Point static files to the helloworld directory.

--- blog/settings.py
+++ blog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -126,6 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
+
+STATICFILES_DIRS = ( os.path.join('helloworld/static'), )
