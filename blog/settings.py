82d2ddb541469319f3b6b6a45e3c7a17d9428e7e
peterlarsen <plarsen@umich.edu>
2018-07-05 18:13:14 -0400

    Minimize requirements.

--- blog/settings.py
+++ blog/settings.py
@@ -88,9 +88,17 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    # Parse database configuration from $DATABASE_URL
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
+
+if (DEBUG):
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+else:
+    DATABASES['default'] = dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 
 
 # Password validation
